a69557ae805f686abe6a410318269073f62f15c1
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-03-01 16:52:16 +0100

    first commit

---
+++ src/main/java/com/pugliatti/andrea/retrofocus/RetrofocusApplication.java
@@ -0,0 +1,13 @@
+package com.pugliatti.andrea.retrofocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrofocusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pugliatti/andrea/retrofocus/RetrofocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pugliatti.andrea.retrofocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
