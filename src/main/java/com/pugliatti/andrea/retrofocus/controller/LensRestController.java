d1349c0f21d20d012ca8845a1ddef4489fe56bb8
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-03-09 18:44:53 +0100

    add rest controller for Lens

---
+++ src/main/java/com/pugliatti/andrea/retrofocus/controller/LensRestController.java
@@ -0,0 +1,40 @@
+package com.pugliatti.andrea.retrofocus.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pugliatti.andrea.retrofocus.model.Lens;
+import com.pugliatti.andrea.retrofocus.service.LensService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/lenses")
+public class LensRestController {
+    private final LensService service;
+
+    public LensRestController(LensService lensService) {
+        this.service = lensService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Lens>> index(@RequestParam(value = "q", required = false) String name) {
+        return new ResponseEntity<>(service.findAllOrByNameContaining(name), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Lens> show(@PathVariable Integer id) {
+        if (!service.existsById(id)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(service.getById(id), HttpStatus.OK);
+    }
+
+}
